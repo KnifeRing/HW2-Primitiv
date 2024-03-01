cd5ab7311640c22018aeae18287c736cb02ea03e
Alexander Khyrbu <rjrednaxela@vk.ru>
2024-03-01 13:58:17 +0300

    rename variables

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 13676; // стоимость билета
-        int y = 20; // кол-во рублей до единицы миль
+        int cost = 13676; // стоимость билета
+        int difference = 20; // кол-во рублей до единицы миль
 
-        int z = (x / y); // кол-во миль
+        int miles = (cost / difference); // кол-во миль
 
-        System.out.println("Количество начисленных миль за купленный билет:" + z);
+        System.out.println("Количество начисленных миль за купленный билет:" + miles);
 
     }
 }
